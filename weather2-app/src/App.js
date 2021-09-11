1858802ecc6605d6071ea9b7dff397c38fd97009
debutler266 <32465329+debutler266@users.noreply.github.com>
2021-09-11 19:13:00 -0400

    added all icons for different weather patterns, & assigned the icons to show properly based on the weather id that matches the weather in each city. This id is based on the api documentation for openweather

--- weather2-app/src/App.js
+++ weather2-app/src/App.js
@@ -25,11 +25,49 @@ class App extends React.Component {
       error: false
     };
     this.getWeather();
+
+    this.weatherIcon = {
+      Thunderstorm:'wi-thunderstorm',
+      Drizzle:'wi-sleet',
+      Rain: 'wi-storm-showers',
+      Snow: 'wi-snow',
+      Atmosphere: 'wi-fog',
+      Clear: 'wi-day-sunny',
+      Clouds: 'wi-day-fog'
+    };
   }
 
   calCelsius(temp){
     let cell = Math.floor(temp - 273.15);
     return cell;
+  }
+
+  get_WeatherIcon(icons,rangeId){
+    switch (true) {
+      case rangeId >= 200 && rangeId <= 232:
+        this.setState({icon: this.weatherIcon.Thunderstorm});
+        break;
+        case rangeId >= 300 && rangeId <= 321:
+          this.setState({icon: this.weatherIcon.Drizzle});
+          break;
+          case rangeId >= 500 && rangeId <= 521:
+            this.setState({icon: this.weatherIcon.Rain});
+            break;
+            case rangeId >= 600 && rangeId <= 622:
+              this.setState({icon: this.weatherIcon.Snow});
+              break;
+              case rangeId >= 701 && rangeId <= 781:
+                this.setState({icon: this.weatherIcon.Atmosphere});
+                break;
+                case rangeId === 800:
+                  this.setState({icon: this.weatherIcon.Clear});
+                  break;
+                  case rangeId >= 801 && rangeId <= 804:
+                    this.setState({icon: this.weatherIcon.Clouds});
+                    break;
+                    default:
+                    this.setState({icon:this.weatherIcon.Clouds})
+    }
   }
 
   //**use api data asyncronisly, also use api key to access secure resources, concatinate string */}
@@ -45,8 +83,13 @@ class App extends React.Component {
     this.setState({
       city:response.name,
       country:response.sys.country,
-      celsius:this.calCelsius(response.main.temp)
+      celsius:this.calCelsius(response.main.temp),
+      temp_max:this.calCelsius(response.main.temp_max),
+      temp_min:this.calCelsius(response.main.temp_min),
+      description:response.weather[0].description
     });
+
+    this.get_WeatherIcon(this.weatherIcon,response.weather[0].id);
   };
 
 
@@ -60,6 +103,7 @@ class App extends React.Component {
       temp_max={this.state.temp_max}
       temp_min={this.state.temp_min}
       description={this.state.description}
+      weatherIcon={this.state.icon}
       />
       </div>
     );
