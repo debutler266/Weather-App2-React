9e0ae1da070d2a0ddc30070566bbea4f2f764354
debutler266 <32465329+debutler266@users.noreply.github.com>
2021-09-11 21:13:23 -0400

    added https extension so app can work on netlify

--- weather2-app/src/App.js
+++ weather2-app/src/App.js
@@ -82,7 +82,7 @@ class App extends React.Component {
 
 
     if(city && country){
-      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`
+      const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`
       );
 
       //*convert api data to JSOn format
